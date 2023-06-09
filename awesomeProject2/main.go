9537e668d7169866032c6adec91dfa1b8e2ca693
gorochxc <128721383+gorochxc@users.noreply.github.com>
2023-06-09 10:27:39 +0700

    Add files via upload

---
+++ awesomeProject2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("[f[f rkjey")
+
+}
+
+type stud struct {
+	student  string
+	ocenca   int
+	opisanie string
+}
+
+var yot stud
+
+func Marshal(v interface{}) ([]byte, error) {
+	yot := stud{"oleg", 5, "norm tip"}
+}
